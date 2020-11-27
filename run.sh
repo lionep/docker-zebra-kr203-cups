12cce0937fe37bf9ead032e71ad13a1cd60f85a7
Lionel Penaud <lpenaud@carlipa.com>
2020-11-27 12:59:00 +0100

    fix(usb)

--- run.sh
+++ run.sh
@@ -14,6 +14,11 @@ DIRECT_PRINTER=`lpinfo -v | grep '^direct' | grep "Zebra%20Technologies" | cut -
 if [ "$DIRECT_PRINTER" != "" ]; then
   echo "Installing $DIRECT_PRINTER"
   lpadmin -p Zebra -E -v "$DIRECT_PRINTER" -P /root/KR203.ppd
+  sleep 5
+  echo "Apply USB policy"
+  # https://wiki.debian.org/CUPSDebugging#Problems_Printing_to_a_USB_Connected_Printer
+  lpadmin -p Zebra -o usb-unidir-default=true
+  lpadmin -p Zebra -o usb-no-reattach-default=true
 else
   echo "Zebra Printer not found."
 fi
